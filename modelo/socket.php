0510467da314d702b88604f327deb5f8889deb13
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-10-29 11:34:14 +0100

    manejo de excepciones en el socket, hace falta cambiarlo en todo

--- modelo/socket.php
+++ modelo/socket.php
@@ -1,16 +1,26 @@
 <?php 
-
+try{
   $host = 'localhost';
   $db = 'prueba';
   $user = 'mau';
   $passwd = getenv('DB_PASSWORD');
+ 
+  $controlador = new mysqli_driver();
+  $controlador->report_mode = MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT;
+
 
   $mysqli = new mysqli($host, $user, $passwd,$db);
-  
-  if ($mysqli->connect_error) {
+
+  echo "<script>console.log('conexion exitosa a la base de datos')</script>";
+
+}catch(mysqli_sql_exception $e){
+
+  die("Error de conexion ". $e->__toString());
+}
+ /* if ($mysqli->connect_error) {
     die("Error de conexion". $mysqli->connect_error);
   }
   
-  echo "<script>console.log('conexion exitosa a la base de datos')</script>";
+echo "<script>console.log('conexion exitosa a la base de datos')</script>";*/
 
 
