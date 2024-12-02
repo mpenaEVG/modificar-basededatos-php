bf46c27a2fe0cae52995a7bbd31227aef9d7efd7
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-12-02 20:50:20 +0100

    añadido punto y coma

--- controlador/registro.php
+++ controlador/registro.php
@@ -20,6 +20,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if($stmt->execute()){
       echo "Usuario Introducido correctamente";
       enviarCorreo($username,$email);
+      header('Location: ../login.php');
     }else {
 
       echo "Error al introducir al usuario";
